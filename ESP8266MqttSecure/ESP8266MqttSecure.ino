77e0ecbfa2e4d56c00cdd17a7cc6e5480a7b68e4
aashay4123 <aashayphirke5798@gmail.com>
2020-08-21 10:39:54 +0530

    improve

--- ESP8266MqttSecure/ESP8266MqttSecure.ino
+++ ESP8266MqttSecure/ESP8266MqttSecure.ino
@@ -1,18 +1,3 @@
-/**
- * Helloworld style, connect an ESP8266 to the IBM IoT Foundation
- * 
- * This is a modified version of the sketch that goes with the developerWorks recipe: https://developer.ibm.com/recipes/tutorials/connect-an-esp8266-with-the-arduino-sdk-to-the-ibm-iot-foundation/
- * 
- * This sketch is modified to show how to use a secure connection between the ESP8266 and the Watson IoT Platform.
- * 
- * A complication is that presently Watson only support TLS v1.2 where as the latest release of the ESP8266 Arduino SDK, 2.3.0 only support TLS v1.1. 
- * The latest ESP8266 code does support TLS v1.2, however as its not come out in an official release yet you need to get the code directly from Github. 
- * Thats a little harder than getting the ESP8266 support with the Arduino Board Manager facility, but its still fairly straight forward and the code 
- * appears stable and for me works well. How to do it is described here: https://github.com/esp8266/Arduino#using-git-version
- * 
- * Author: Ant Elder
- * License: Apache License v2
- */
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h> 
 
@@ -21,7 +6,7 @@ const char* ssid = "<yourSSID>";
 const char* password = "<yourPassword>";
 
 #define ORG "quickstart" // "quickstart" or use your organisation
-#define DEVICE_ID "Test5"      
+#define DEVICE_ID "Test1s"      
 #define DEVICE_TYPE "ESP8266" // your device type or not used for "quickstart"
 #define TOKEN "xxxxxxxxxxxxx" // your device token or not used for "quickstart"
 //-------- Customise the above values --------
